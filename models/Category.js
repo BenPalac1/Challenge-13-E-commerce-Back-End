9436373142bd5c22057a997f8165d816c779fc94
BenPalac1 <ben.palacpac@gmail.com>
2024-01-06 22:35:04 -0500

    defined columns in models, updated server.js to import sequelize connection

--- models/Category.js
+++ models/Category.js
@@ -8,11 +8,14 @@ Category.init(
   {
     // define columns
     id: {
-
+      type: DataTypes.INTEGER, // set datatype to INT
+      allowNull: false, // this value cannot be null
+      primaryKey: true, // set primary key
+      autoIncrement: true, 
     },
 
     category_name: {
-
+      type: DataTypes.STRING,
     },
   },
   
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -8,14 +8,26 @@ ProductTag.init(
   {
     // define columns
     id:  {
-
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
 
     tag_id: {
-
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
     },
-    product_id: {
 
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
     },
   },
 
